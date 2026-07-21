c1a7752f23b388eddab38d2baae912612a66045b
Myong Hyo You <myonghyoyou@gmail.com>
2026-07-22 01:05:16 +0900

    [FIX] GovernorStatsResponse xAxisList 직렬화 키 오류 수정

--- backend/src/main/java/com/trendone/govtrend/dto/governor/GovernorStatsResponse.java
+++ backend/src/main/java/com/trendone/govtrend/dto/governor/GovernorStatsResponse.java
@@ -3,6 +3,8 @@ package com.trendone.govtrend.dto.governor;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,6 +12,8 @@ import lombok.Data;
 @AllArgsConstructor
 public class GovernorStatsResponse {
 
+    @JsonProperty("xAxisList")
     private List<String> xAxisList;
+
     private Map<String, GovernorStatItem> statDataObj;
 }
